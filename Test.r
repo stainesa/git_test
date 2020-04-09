c318c1d843c8e88434ac4b614efe8ad3198196e2
stainesa <anthony.staines@gmail.com>
2020-04-09 17:34:35 +0100

    Shell code added

--- Test.r
+++ Test.r
@@ -1,1 +1,7 @@
-# This is a test file
+# This is a test file yes it is
+# no it isn't
+git remote add origin https://github.com/stainesa/git_test
+git config remote.origin.url git@github.com:stainesa/git_test
+git pull -u origin master
+git push -u origin master
+
